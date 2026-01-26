07e6b0e412a46525026257d8b4cad2fbbed7e8fd
DikyVinus <137989414+DikyVinus@users.noreply.github.com>
2026-01-26 16:21:15 +0700

    Update action.sh

--- module/action.sh
+++ module/action.sh
@@ -20,10 +20,10 @@ else
 fi
 
 echo
-echo "--- cron.log (last 10, filtered) ---"
+echo "--- cron.log (last 10) ---"
 if [ -f "$CRONLOG" ]; then
-    tail -n 50 "$CRONLOG" | \
-        grep -Ev 'Profile (CPUSET|SCHED)_SP_TOP_APP' | \
+    tail -n 100 "$CRONLOG" | \
+        grep -Ev 'Profile (CPUSET|SCHED)_SP_TOP_APP|Profile (CPUSET|SCHED)_SP_(RESTRICTED|BACKGROUND)' | \
         tail -n 10
 else
     echo "(cron.log not found)"
