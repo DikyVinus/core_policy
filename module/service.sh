2c8e07e028552ed70a2fa66aa25be6c5eb7177a0
DikyVinus <137989414+DikyVinus@users.noreply.github.com>
2026-01-28 13:17:12 +0700

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -114,8 +114,7 @@ EOF
         chmod 0600 "$CRONTAB"
     fi
 
-    $SU "cd '$CRONDIR' && busybox crond -f -c '$CRONDIR' -L '$CRONLOG'" &
-
+    busybox crond -c "$CRONDIR" -L "$CRONLOG" &
     sleep 1
     CRON_PID="$(pgrep -f "busybox crond.* $CRONDIR" | head -n1)"
     if [ -n "$CRON_PID" ]; then
