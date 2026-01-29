de7f2dcc8eea5266da143c3c5d7ec0bdef7e9f27
DikyVinus <137989414+DikyVinus@users.noreply.github.com>
2026-01-29 16:45:06 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -2,7 +2,7 @@
 exec 2>/dev/null
 
 BB="$(command -v busybox)" || exit 0
-BINDIR="$(dirname "$(readlink -f "$BB" 2>/dev/null || echo "$BB")")"
+BINDIR="$(dirname "$(readlink -f "$BB" || echo "$BB")")"
 
 for name in core_policy_discovery core_policy_exe core_policy_demote; do
     target="$BINDIR/$name"
