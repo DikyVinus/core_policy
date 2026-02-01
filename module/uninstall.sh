cf4628b48e05ef45ea88d1b918c0f0af1a290570
DikyVinus <137989414+DikyVinus@users.noreply.github.com>
2026-02-01 15:33:03 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -2,11 +2,24 @@
 exec 2>/dev/null
 
 APP_PKG="com.coreshift.policy"
+APP_SERVICE="$APP_PKG/.TopAppService"
+
+ENABLED="$(cmd settings get secure enabled_accessibility_services)"
+
+case "$ENABLED" in
+    *"$APP_SERVICE"*)
+        NEW="$(echo "$ENABLED" | sed "s#:$APP_SERVICE##; s#$APP_SERVICE:##; s#$APP_SERVICE##")"
+        cmd settings put secure enabled_accessibility_services "$NEW"
+        ;;
+esac
+
+[ "$ENABLED" = "$APP_SERVICE" ] && cmd settings put secure accessibility_enabled 0
 
 cmd package uninstall "$APP_PKG"
 
 BB="$(command -v resetprop)" || exit 0
-BINDIR="$(dirname "$(readlink -f "$BB" || echo "$BB")")"
+BB="$(readlink -f "$BB" || echo "$BB")"
+BINDIR="$(dirname "$BB")"
 
 for name in core_policy_discovery core_policy_exe core_policy_demote; do
     target="$BINDIR/$name"
@@ -19,5 +32,3 @@ for name in core_policy_discovery core_policy_exe core_policy_demote; do
         esac
     fi
 done
-
-exit 0
